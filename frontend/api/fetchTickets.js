c4d9a2ef908ef4dd867f46b124874347f3da65e0
Parker Rudd <parkerbrudd@gmail.com>
2024-04-21 00:45:55 -0600

    Removed logs

--- frontend/api/fetchTickets.js
+++ frontend/api/fetchTickets.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default fetchTickets = async () => {
-  const res = await axios.get("https://192.168.86.29/tickets");
+  const res = await axios.get("https://help-desk-akxg.onrender.com/tickets");
   return res.data;
 };
